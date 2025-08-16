9b163d167e8250a0bcf0086ce0e2009b21f92b2b
pppoh <2ndPipatpong987123@gmail.com>
2025-08-16 13:47:27 +0700

    OS / move file *.txt to CaseStudy01

--- OS/CaseStudy01/obj/Debug/net9.0/CaseStudy01.AssemblyInfo.cs
+++ OS/CaseStudy01/obj/Debug/net9.0/CaseStudy01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378a12c0a59a1a202c611b8f5484eae9fd5c0964")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c61f5f1077a3848aba7927ebd8a0ee57ae5626")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaseStudy01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
